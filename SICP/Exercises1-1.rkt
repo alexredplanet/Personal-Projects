3e3902b9fdf99b3759cb3434d2eb184c4fd9b638
Alex Mars <alexmars@Alexs-MacBook-Pro.local>
2022-01-01 20:21:51 +1100

    Added exercises 1.7 and 1.8

--- SICP/Exercises1-1.rkt
+++ SICP/Exercises1-1.rkt
@@ -97,3 +97,42 @@
 ;; thus in the sqrt procedure, the else clause of the cond will be recursively
 ;; evaluated using the new-if definition, leading to an infinite loop.
 
+;; Exercise 1.7
+;; The good-enough? test will fail for small numbers due to floating point imprecision
+;; This makes it inaccurate for small numbers
+(square (sqrt 0.00000001))
+;; For large numbers, the result may never fall within the threshold
+; (sqrt 100000000000000000000000000000000000) won't finish
+; this is due to the rounding error associated with storing large numbers
+; if a number is too large, the difference between subsequent guesses will never
+; be less than 0.001 due to the rounding error in thier representations.
+
+; Alternative good-enough?:
+; stop when the change in guess is a very small fraction of the guess
+(define (sqrt-iter2 guess x)
+  (if (good-enough?2 (improve guess x) guess x)
+      guess
+      (sqrt-iter2 (improve guess x) x)))
+
+(define (good-enough?2 guess previous x)
+  (< (abs (- guess previous)) (* guess 0.001)))
+
+(define (sqrt2 x)
+  (sqrt-iter2 1.0 x))
+
+(square (sqrt2 0.00000001)) ; returns a much more accurate answer
+; sqrt2 also finishes with a more accurate answer
+
+; Exercise 1.8 Newton's Method for cube roots
+(define (cube-root-iter guess x)
+  (if (good-enough?2 (improve-cube guess x) guess x)
+      guess
+      (cube-root-iter (improve-cube guess x) x)))
+
+(define (improve-cube guess x)
+  (/ (+ (/ x (square guess)) (* 2 guess)) 3))
+
+(define (cube-root x)
+  (cube-root-iter 1.0 x))
+
+(cube-root 27)
